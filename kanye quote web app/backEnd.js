0f7e415bd03c93d86f53129b15c8209fda510871
Genevieve Ferguson <54678225+gaf17@users.noreply.github.com>
2022-02-09 12:56:42 -0500

    Delete kanye quote web app directory

--- kanye quote web app/backEnd.js
+++
@@ -1,7 +0,0 @@
-let quotesDiv = document.getElementById('quotes')
-
-fetch('https://api.kanye.rest')
-.then(res => res.json())
-.then(quote => {
-    quotesDiv.innerHTML += `<p> ${quote.quote} </p>`
-})
