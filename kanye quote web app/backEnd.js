b5fa73530ab8469652300eb181ddf0dc0b64e348
Genevieve Ferguson <54678225+gaf17@users.noreply.github.com>
2022-02-09 12:35:29 -0500

    Add files via upload

---
+++ kanye quote web app/backEnd.js
@@ -0,0 +1,7 @@
+let quotesDiv = document.getElementById('quotes')
+
+fetch('https://api.kanye.rest')
+.then(res => res.json())
+.then(quote => {
+    quotesDiv.innerHTML += `<p> ${quote.quote} </p>`
+})
